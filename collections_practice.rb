e9694ce33d6f134881c6ba9b0b7b561ab532d525
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-16 20:33:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ def kesha_maker(array)
 end
 
 def find_a(array)
-  new_array = array.collect { |i| i.match(/^a[a-z]+/)
+  new_array = array.collect { |i| i.match(/^a[a-z]+/)}
 end
 
 def sum_array(array)
