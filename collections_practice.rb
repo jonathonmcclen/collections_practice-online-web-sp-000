0478c96893c0c5a8ce5a7b6d5fce47f2deeac31d
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-16 20:24:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
+
+def swap_elements(array)
+  save = array[1]
+  array[1] = array[2]
+  array[2] = save
+end
+
+def reverse_array(array)
+  
+end
+
+def kesha_maker(array)
+  
+end
+
+def find_a(array)
+  
+end
+
+def sum_array(array)
+  sum = 0 
+  array.each do |num|
+    sum += num
+  end 
+  sum
+end
+
+def add_s(array)
+  
+end
